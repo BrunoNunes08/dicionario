a56e61acd84f846c28eb640433879ef70dea9858
BrunoNunes08 <133694839+BrunoNunes08@users.noreply.github.com>
2023-11-28 17:26:00 -0300

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,69 @@
+// let nomePersonagem = "Carlos";
+// let sobrenomePersonagem = "Adamastor Pequeno";
+// let idadePersonagem = 325;
+// let emEmprego = false;
+
+// console.log(`${nomePersonagem} ${sobrenomePersonagem} tem ${idadePersonagem} anos de idade e ${emEmprego?"":"não "}está empregado`)
+
+// --------------------------------------------------------------
+
+// let nome = prompt("Digite seu nome");
+// let idade = prompt("Digite sua idade");
+
+// console.log(typeof nome, typeof idade);
+
+// --------------------------------------------------------------
+
+// const num = 123;
+// const numString = num.toString();
+// console.log(typeof numString);
+
+// --------------------------------------------------------------
+
+// const stringNum = "123";
+// const stringNumString = Number(stringNum);
+// console.log(typeof stringNumString);
+
+// --------------------------------------------------------------
+
+// let nome, cidade;
+// console.log(typeof nome, typeof cidade);
+// nome = prompt("Digite seu nome");
+// cidade = prompt("Digite sua cidade");
+// console.log(typeof nome, typeof cidade);
+// console.log(`Olá, meu nome é ${nome} e eu moro em ${cidade}`);
+
+// --------------------------------------------------------------
+
+// let pergunta1 = "Você gosta de abacate? ";
+// let pergunta2 = "Você já viajou para Londres? ";
+// let pergunta3 = "Você já experimentou batata rústica? ";
+// let resposta1 = prompt(pergunta1);
+// let resposta2 = prompt(pergunta2);
+// let resposta3 = prompt(pergunta3);
+
+// console.log(pergunta1, resposta1)
+// console.log(pergunta2, resposta2)
+// console.log(pergunta3, resposta3)
+
+// --------------------------------------------------------------
+
+// let x = 22;
+// let y = 17;
+// let z = x;
+
+// x = y;
+// y = z;
+
+// console.log(x, y)
+
+// --------------------------------------------------------------
+
+let num1 = Number(prompt("Digite um número"));
+let num2 = Number(prompt("Digite outro número"));
+
+let soma = num1 + num2;
+let multiplicacao = num1 * num2;
+
+console.log("1. A soma dos números é: ", soma);
+console.log("1. A multiplicação dos números é: ", multiplicacao);
